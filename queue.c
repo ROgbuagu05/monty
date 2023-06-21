f17c5703adb8f322a26ab2be6e016c783edd2df1
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-21 04:08:10 -0700

    queue.c

---
+++ queue.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "monty.h"
+/**
+ * queue - converts queue to stack
+ * @stack: Double pointer to the head of the stack
+ * @line_number: Line number of the opcode in the file.
+ */
+void queue(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
