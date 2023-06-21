4e5d224d97b28845cba64bf2246511a1a72d3a84
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-21 04:00:15 -0700

    stack.c

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "monty.h"
+/**
+ * stack - converts queue to stack
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number of the opcode in the file.
+ */
+void stack(stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+	(void) stack;
+
+	/* Do nothing, stack is already set to LIFO */
+}
